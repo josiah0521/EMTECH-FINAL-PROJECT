308f32d1dce4b0fcda8c7854f904af91e22f0d74
josiah0521 <131630473+josiah0521@users.noreply.github.com>
2024-05-18 12:11:03 +0800

    1Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def load_model():
   return model
 model=load_model()
 st.write("""
-# Plant Leaf Detection System"""
+# Flowers Detection Classifier"""
 )
 file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 
